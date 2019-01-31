8a63ea46d630f18f649c57382419df60731df577
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 22:39:23 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-  #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize(:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width)
+
+ attributes.each {|key, value| self.send(("#{key}="), value)}
+ 
+ person.send("#{method_name}=", value)
+  
+  end
+  
+
 end
